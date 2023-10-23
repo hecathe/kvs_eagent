0f2e047849b83a80e8e06faea547b4c76a367d76
Екатерина Филиппова <catherina1punk@gmail.com>
2023-10-23 16:41:58 +0300

    setup

---
+++ components/HighlightedText.vue
@@ -0,0 +1,14 @@
+<template>
+	<h2 style="font-weight: 800">"{{ text }}"</h2>
+</template>
+
+<script>
+export default {
+	props: {
+		text: {
+			type: String,
+			default: 'Some Message',
+		},
+	},
+};
+</script>
---
+++ pages/about.vue
@@ -0,0 +1,9 @@
+<template>
+	<div>
+		<h1>This is the about page</h1>
+
+		<HighlightedText></HighlightedText>
+	</div>
+</template>
+
+<script></script>
