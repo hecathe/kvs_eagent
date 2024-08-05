c258a4ef8c76e0449c03cb6f48b25fe8834ad584
Екатерина Филиппова <catherina1punk@gmail.com>
2024-08-05 12:45:37 +0300

    deploy 1.2

--- output/public/_nuxt/icons.44a1e6af.js
+++
@@ -1,1 +0,0 @@
-import"./entry.7c7d29ac.js";const t=""+new URL("icons.51ddd4cb.svg",import.meta.url).href;export{t as default};
--- output/public/_nuxt/svg-icon.e6f29d5d.js
+++
@@ -1,1 +0,0 @@
-import{_ as m}from"./svg-icon.vue.8979bfa3.js";import"./entry.7c7d29ac.js";export{m as default};
